010fa0d922e8b35690123d614303cc2d5f8d2241
Ever <said26gr@gmail.com>
2020-04-12 19:02:53 -0500

    Programa IVA

---
+++ iva.py
@@ -0,0 +1,8 @@
+# precio de producto
+# Muestre valor del IVA
+
+print ('Calcular el IVA')
+precio=float(input ('Da el precio del producto: '))
+
+Iva=precio*0.16
+print('El iva del producto es: ',Iva)
